7f3ed8eedf7c81a988abd57fc1051cc6630a0e59
raseledutools <bhmrasel60@gmail.com>
2026-07-31 13:08:56 +0600

    fix(drive): add missing import for rememberLauncherForActivityResult

--- app/src/main/java/com/rasel/RasFocus/drivebackup/DriveFileManagerActivity.kt
+++ app/src/main/java/com/rasel/RasFocus/drivebackup/DriveFileManagerActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
