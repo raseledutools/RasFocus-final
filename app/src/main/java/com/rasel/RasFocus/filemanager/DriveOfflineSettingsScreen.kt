6582204ad6d7cfe9151af877af7c10356a150872
Rasel Mia <bhmrasel60@gmail.com>
2026-08-15 22:19:09 +0600

    feat: teal header color for DriveOfflineSettings

--- app/src/main/java/com/rasel/RasFocus/filemanager/DriveOfflineSettingsScreen.kt
+++ app/src/main/java/com/rasel/RasFocus/filemanager/DriveOfflineSettingsScreen.kt
@@ -171,7 +171,7 @@ fun DriveOfflineSettingsScreen(onBack: () -> Unit) {
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color(0xFF1A73E8),
+                    containerColor = Color(0xFF00796B),
                     titleContentColor = Color.White,
                     navigationIconContentColor = Color.White
                 )
